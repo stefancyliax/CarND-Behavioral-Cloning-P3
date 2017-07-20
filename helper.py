50cee485fdd3845d8574531943bafe27e0a6335e
Unknown <stcyliax@gmail.com>
2017-07-20 19:03:01 +0200

    Pipeline works now
    next step: gathering good data, train on GPU

---
+++ helper.py
@@ -0,0 +1,134 @@
+import numpy as np
+import pandas as pd
+from skimage import transform, color
+from keras.preprocessing.image import load_img
+
+
+def read_csv():
+    driving_log = pd.read_csv('../SDCND_output/driving_log.csv', header=None,
+                              names=['Center Image', 'Left Image', 'Right Image', 'Steering Angle',
+                                     'Throttle', 'Break', 'Speed'])
+    return driving_log
+
+
+def clean_data(driving_log, upper_angle=0.7, zero_frac=0.1):
+    # clean the driving log data:
+    # 1. remove most but not all samples driving straight to reduce bias of going straight
+    # 2. remove samples with large steering angle
+
+    a = driving_log.shape[0]
+
+    # Safe small number of sample of going straight
+    zero_lines = driving_log[driving_log['Steering Angle'] == 0]
+    b = zero_lines.shape[0]
+    zero_lines_sample = zero_lines.sample(frac=zero_frac, random_state=42)
+    c = zero_lines_sample.shape[0]
+
+    # Drop all samples of driving straight
+    driving_log = driving_log[driving_log['Steering Angle'] != 0]
+    d = driving_log.shape[0]
+
+    # Drop samples with large steering angles left and right
+    driving_log = driving_log[driving_log['Steering Angle'] < upper_angle]
+    driving_log = driving_log[driving_log['Steering Angle'] > - upper_angle]
+    e = driving_log.shape[0]
+
+    # Add some samples of driving straight back in
+    driving_log = pd.concat([driving_log, zero_lines_sample])
+    f = driving_log.shape[0]
+
+    # Print statistics
+    print('Number of samples in input data:', a)
+    print('Number of samples going straight:', b)
+    print('Number of random samples going straight that are kept (', zero_frac * 100, '% ):', c)
+    print('Number of samples without samples going straight:', d)
+    print('Without samples with large steering angles ( larger than +-', upper_angle, '):', e)
+    print('Number of samples with rescued samples: ', f)
+
+    return driving_log
+
+
+def load_and_augment_image(sample):
+    # Run randomize to determine what kind of augmentation is used.
+    # There are 4 kinds of augmentations with 3 ways each
+    # 1. Camera: left image | center image | right image
+    # 2. Flip: normal | flipped
+    # 3. Horizontal shift: left | normal | right
+    # 4. Vertical shift: up | normal | down
+    # 5. Brightness: bright | normal | dark
+    rand = np.random.random(5)
+
+    # 1. Camera: left image | center image | right image
+    image, steering_angle = load_image(sample, rand[0])
+
+    # 2. Flip: normal | flipped
+    image, steering_angle = flip_image(image, steering_angle, rand[1])
+
+    # 3. Horizontal shift: left | normal | right
+    image = h_shift_image(image, rand[2])
+
+    # 4. Vertical shift: up | normal | down
+    image = v_shift_image(image, rand[3])
+
+    # 5. Brightness: bright | normal | dark
+    image = brightness_image(image, rand[4])
+
+    return image, steering_angle
+
+
+def load_image(sample, rand=0.5, steering_correction=0.25):
+    # Load center, left or right image based on rand
+    steering_angle = float(sample[3])
+    if rand < 1 / 3:
+        # Left image
+        image_path = '../SDCND_output/IMG/' + sample[1].split('\\')[-1]
+        steering_angle += steering_correction
+    elif rand > 2 / 3:
+        # Right image
+        image_path = '../SDCND_output/IMG/' + sample[2].split('\\')[-1]
+        steering_angle -= steering_correction
+    else:
+        # Center image
+        image_path = '../SDCND_output/IMG/' + sample[0].split('\\')[-1]
+
+    # Load image and steering angle
+    image = load_img(image_path)
+    return image, steering_angle
+
+
+def flip_image(img, angle, rand=1):
+    if rand < 1 / 2:
+        img = np.fliplr(img)
+        angle = angle * -1.0
+    return img, angle
+
+
+def brightness_image(img, rand=0.5):
+    img_hsv = color.rgb2hsv(img)
+    if rand < 1 / 3:
+        img_hsv[:, :, 2] = img_hsv[:, :, 2] - 0.3
+    elif rand > 2 / 3:
+        img_hsv[:, :, 2] = img_hsv[:, :, 2] + 0.3
+    img_hsv[:, :, 2] = np.clip(img_hsv[:, :, 2], 0, 1)
+    img = color.hsv2rgb(img_hsv)
+    return img
+
+
+def v_shift_image(img, rand=0.5):
+    if rand < 1 / 3:
+        tform = transform.AffineTransform(rotation=0, shear=0, translation=(0, 20))
+        img = transform.warp(img, tform)
+    elif rand > 2 / 3:
+        tform = transform.AffineTransform(rotation=0, shear=0, translation=(0, -20))
+        img = transform.warp(img, tform)
+    return img
+
+
+def h_shift_image(img, rand=0.5):
+    if rand < 1 / 3:
+        tform = transform.AffineTransform(rotation=0, shear=0, translation=(20, 0))
+        img = transform.warp(img, tform)
+    elif rand > 2 / 3:
+        tform = transform.AffineTransform(rotation=0, shear=0, translation=(-20, 0))
+        img = transform.warp(img, tform)
+    return img
--- model.py
+++ model.py
@@ -1,86 +1,82 @@
-import numpy as np
-import cv2
-import csv
-import pickle
+import os
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
 
-from keras.models import Sequential
-from keras.layers import Flatten, Dense, Conv2D, Dropout, MaxPooling2D, Activation, Lambda, Cropping2D
-from keras.preprocessing.image import load_img
+from keras.models import Sequential, load_model
+from keras.layers import Flatten, Dense, Conv2D, Dropout, Lambda, Cropping2D
+from keras.preprocessing.image import img_to_array
+
+from helper import *
 
 
-# TODO: 1x1 convolutions to automatically get best color model
-# TODO: 0. drive.py sends RGB images to the model; cv2.load_img() reads images in BGR format!!!!
-# TODO: Horizontal and vertical shifts
-# TODO: Data augmentation: brightness
+# DONE: 1x1 convolutions to automatically get best color model
+# DONE: 0. drive.py sends RGB images to the model; cv2.load_img() reads images in BGR format!!!!
+# DONE: Horizontal and vertical shifts
+# DONE: Data augmentation: brightness
 # DONE: Remove data with steering angle =0
-# TODO: Transfer learniong: Save model and load model every time
+# dONE: Transfer learning: Save model and load model every time
 # TODO: Stacked histogram for visualisation
+# TODO: use random number to make augmentation random in amount
 
 
+def train_generator(data, batch_size=32, augments_per_sample=8):
+    nb_data = len(data)
+    while 1:
+        shuffle(data)
+        for offset in range(0, nb_data, batch_size):  # loop over batches
+            batch_samples = data.values[offset:offset + batch_size]
 
-def read_csv():
-    csv_data = []
-    with open('../SDCND_output/driving_log.csv') as csvfile:
-        reader = csv.reader(csvfile)
-        for line in reader:
-            csv_data.append(line)
+            images = []
+            steering_angles = []
+            for batch_sample in batch_samples:  # loop over samples in batch
+                for n in range(augments_per_sample):  # augmentations per sample
+                    # Run randomizer to determine what kind of augmentation is used.
+                    # There are 4 kinds of augmentations with 3 ways each
+                    # 1. Camera: left image | center image | right image
+                    # 2. Flip: normal | flipped
+                    # 3. Horizontal shift: left | normal | right
+                    # 4. Vertical shift: up | normal | down
+                    # 5. Brightness: bright | normal | dark
 
-    return csv_data
+                    image, steering_angle = load_and_augment_image(batch_sample)
 
+                    images.append(img_to_array(image))
+                    steering_angles.append(steering_angle)
 
-def import_pictures(csv_data, steering_correction=0.25):
-    images = []
-    steering_angle = []
-    for line in csv_data:
-        # Center image
-        image_path = '../SDCND_output/IMG/' + line[0].split('\\')[-1]
-        images.append(load_img(image_path))
-        steering_angle.append(float(line[3]))
-        # Left image
-        image_path = '../SDCND_output/IMG/' + line[1].split('\\')[-1]
-        images.append(load_img(image_path))
-        steering_angle.append(float(line[3]) + steering_correction)
-        # Right image
-        image_path = '../SDCND_output/IMG/' + line[2].split('\\')[-1]
-        images.append(load_img(image_path))
-        steering_angle.append(float(line[3]) - steering_correction)
-
-    X_train = np.array(images)
-    y_train = np.array(steering_angle)
-    print('After import shape X_train: ', X_train.shape, ',  Shape y_train: ', y_train.shape)
-
-    return X_train, y_train
+            X_train = np.array(images)
+            y_train = np.array(steering_angles)
+            yield shuffle(X_train, y_train)
 
 
-def augment_pictures(X_train, y_train):
-    print('Before Shape X_train: ', X_train.shape, ',  Shape y_train: ', y_train.shape)
+def validation_generator(data, batch_size=32):
+    nb_data = len(data)
+    while 1:
+        shuffle(data)
+        for offset in range(0, nb_data, batch_size):  # loop over batches
+            batch_samples = data.values[offset:offset + batch_size]
 
-    X_aug, y_aug = [], []
-    for X, y in zip(X_train, y_train):
-        X_aug.append(X)
-        y_aug.append(y)
-        X_aug.append(cv2.flip(X, 1))
-        y_aug.append(y * -1.0)
+            images = []
+            steering_angles = []
+            for batch_sample in batch_samples:  # loop over samples in batch
+                # don't make any augmentations, just load the data
+                image, steering_angle = load_image(batch_sample)
 
-    X_aug = np.array(X_aug)
-    y_aug = np.array(y_aug)
-    print('After Shape X_train: ', X_aug.shape, ',  Shape y_train: ', y_aug.shape)
+                images.append(img_to_array(image))
+                steering_angles.append(steering_angle)
 
-    return X_aug, y_aug
-
-
-# X_train, y_train = import_pictures()
+            X_valid = np.array(images)
+            y_valid = np.array(steering_angles)
+            yield shuffle(X_valid, y_valid)
 
 
-
-def nvidia_model(X_train, y_train, EPOCHS=5):
+def nvidia_model():
     model = Sequential()
     model.add(Cropping2D(cropping=((62, 25), (0, 0)), input_shape=(160, 320, 3)))
     model.add(Lambda(lambda x: x / 255.0 - 0.5))
+
+    # 1x1 convolution layer to automatically determine best color model
+    model.add(Conv2D(3, 1, 1, subsample=(1, 1), activation='relu'))
 
     model.add(Conv2D(24, 5, 5, subsample=(2, 2), activation='relu'))
     model.add(Conv2D(36, 5, 5, subsample=(2, 2), activation='relu'))
@@ -89,19 +85,68 @@ def nvidia_model(X_train, y_train, EPOCHS=5):
     model.add(Conv2D(64, 3, 3, subsample=(1, 1), activation='relu'))
     model.add(Flatten())
     model.add(Dense(100, activation='relu'))
+    model.add(Dropout(0.5))
     model.add(Dense(50, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(10, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1))
+    model.compile(optimizer='adam', loss='mse')
+
+    print('Using Nvidia model with dropout')
+    return model
+
+
+def simple_model():
+    model = Sequential()
+    model.add(Cropping2D(cropping=((62, 25), (0, 0)), input_shape=(160, 320, 3)))
+    model.add(Lambda(lambda x: x / 255.0 - 0.5))
+    model.add(Flatten())
     model.add(Dense(10, activation='relu'))
     model.add(Dense(1))
     model.compile(optimizer='adam', loss='mse')
 
-    history_object = model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=EPOCHS, verbose=1)
+    print('Using simple model!')
+    return model
 
+
+if __name__ == "__main__":
+
+    # hyperparameters
+    EPOCHS = 1
+    augments_per_sample = 8
+
+    # load, clean and split data
+    driving_log = read_csv()
+    driving_log = clean_data(driving_log)
+    train_samples, validation_samples = train_test_split(driving_log, test_size=0.2)
+
+    # Instantiate generators and model
+    gen_train = train_generator(train_samples, batch_size=32, augments_per_sample=augments_per_sample)
+    gen_valid = validation_generator(validation_samples, batch_size=32)
+    #gen_train = validation_generator(train_samples, batch_size=32)
+
+    # transfer learning: if there is already a model, load it. If not, instantiate new model.
+    if os.path.exists('model.h5'):
+        model = load_model('model.h5')
+        print('Previous model loaded!')
+    else:
+        model = nvidia_model()
+
+    # train model using generators
+    history_object = model.fit_generator(generator=gen_train,
+                                         samples_per_epoch=len(train_samples)*augments_per_sample,
+                                         validation_data=gen_valid,
+                                         nb_val_samples=len(validation_samples),
+                                         verbose=1,
+                                         nb_epoch=EPOCHS)
+    # save model
     model.save('model.h5')
 
-    ### print the keys contained in the history object
+    # print the keys contained in the history object
     print(history_object.history.keys())
 
-    ### plot the training and validation loss for each epoch
+    # plot the training and validation loss for each epoch
     plt.plot(history_object.history['loss'])
     plt.plot(history_object.history['val_loss'])
     plt.title('model mean squared error loss')
@@ -109,40 +154,3 @@ def nvidia_model(X_train, y_train, EPOCHS=5):
     plt.xlabel('epoch')
     plt.legend(['training set', 'validation set'], loc='upper right')
     plt.show()
-
-
-def generator(data, batch_size=32, steering_correction=0.25):
-    # Note that a batch size results in three times the images for learning due to the left and right images
-    # being used as well
-    num_data = len(data)
-    while 1:
-        shuffle(data)
-        for offset in range(0, num_data, batch_size):
-            batch_samples = data[offset:offset + batch_size]
-
-            images = []
-            steering_angle = []
-            for batch_sample in batch_samples:
-                # Center image
-                image_path = '../SDCND_output/IMG/' + batch_sample[0].split('\\')[-1]
-                images.append(load_img(image_path))
-                steering_angle.append(float(batch_sample[3]))
-                # Left image
-                image_path = '../SDCND_output/IMG/' + batch_sample[1].split('\\')[-1]
-                images.append(load_img(image_path))
-                steering_angle.append(float(batch_sample[3]) + steering_correction)
-                # Right image
-                image_path = '../SDCND_output/IMG/' + batch_sample[2].split('\\')[-1]
-                images.append(load_img(image_path))
-                steering_angle.append(float(batch_sample[3]) - steering_correction)
-
-            X_train = np.array(images)
-            y_train = np.array(steering_angle)
-            yield shuffle(X_train, y_train)
-
-
-csv_data = read_csv()
-train_samples, validation_samples = train_test_split(csv_data, test_size=0.2)
-
-X_train, y_train = import_pictures(csv_data)
-X_train, y_train = augment_pictures(X_train, y_train)
